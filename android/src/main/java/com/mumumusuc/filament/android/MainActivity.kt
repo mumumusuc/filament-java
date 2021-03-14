1c54fa78810505c81caee99625db67e81065d9d9
mumumusuc <mumumusuc@gitee.com>
2021-03-14 16:34:38 +0800

    setup Android & Desktop

--- android/src/main/java/com/mumumusuc/filament/android/MainActivity.kt
+++ android/src/main/java/com/mumumusuc/filament/android/MainActivity.kt
@@ -2,9 +2,16 @@ package com.mumumusuc.filament.android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.filament.Filament
 
-class MainActivity : AppCompatActivity() {
+class FilamentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    companion object {
+        init {
+            Filament.init()
+        }
     }
 }
--- desktop/src/main/java/com/mumumusuc/filament/desktop/DesktopLauncher.kt
+++ desktop/src/main/java/com/mumumusuc/filament/desktop/DesktopLauncher.kt
@@ -3,11 +3,13 @@ package com.mumumusuc.filament.desktop
 class DesktopLauncher {
     companion object {
         @JvmStatic
+        @Suppress("unused")
         fun main(args: Array<String>) {
             println("hello world")
         }
 
         init {
+            System.loadLibrary("awt")
             System.loadLibrary("filament-jni")
         }
     }
