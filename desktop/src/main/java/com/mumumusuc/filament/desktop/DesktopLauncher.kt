b41b7b50480b1b694c45aa1a10a4dd46420b5b2f
mumumusuc <mumumusuc@gitee.com>
2021-03-15 12:59:13 +0800

    add sample to Android & Desktop

--- desktop/src/main/java/com/mumumusuc/filament/desktop/DesktopLauncher.kt
+++ desktop/src/main/java/com/mumumusuc/filament/desktop/DesktopLauncher.kt
@@ -1,16 +1,63 @@
 package com.mumumusuc.filament.desktop
 
 import com.mumumusuc.filament.sample.HelloWorld
+import java.awt.Canvas
+import java.awt.Dimension
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import javax.swing.JFrame
+
+private const val kWindowWidth = 400
+private const val kWindowHeight = 400
 
 class DesktopLauncher {
+    private var mQuit = false
+    private val mCanvas: Canvas = Canvas().apply {
+        addComponentListener(object : ComponentAdapter() {
+            override fun componentResized(e: ComponentEvent) {
+                // TODO
+            }
+        })
+        preferredSize = Dimension(kWindowWidth, kWindowHeight)
+        isVisible = true
+    }
+
+    init {
+        JFrame(javaClass.simpleName).run {
+            addWindowListener(object : WindowAdapter() {
+                override fun windowClosing(e: WindowEvent) {
+                    mQuit = true
+                }
+            })
+            defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            setLocationRelativeTo(null)
+            add(mCanvas)
+            pack()
+            isResizable = false
+            isVisible = true
+        }
+    }
+
+    fun start() {
+        HelloWorld().run {
+            onCreate()
+            onAttach(mCanvas)
+            onResized(mCanvas.width, mCanvas.height)
+            while (!mQuit) {
+                onRender()
+                Thread.sleep(16)
+            }
+            onDetach()
+            onDestroy()
+        }
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            HelloWorld().run {
-                onCreate()
-                onRender()
-                onDestroy()
-            }
+            DesktopLauncher().start()
         }
 
         init {
--- sample/src/main/java/com/mumumusuc/filament/sample/HelloWorld.kt
+++ sample/src/main/java/com/mumumusuc/filament/sample/HelloWorld.kt
@@ -1,46 +1,14 @@
 package com.mumumusuc.filament.sample
 
 import com.google.android.filament.*
-import java.awt.Canvas
-import java.awt.Dimension
-import java.awt.event.WindowAdapter
-import java.awt.event.WindowEvent
-import javax.swing.JFrame
-import javax.swing.JFrame.EXIT_ON_CLOSE
-
-private const val kWindowWidth = 400
-private const val kWindowHeight = 400
 
 class HelloWorld {
     private lateinit var mEngine: Engine
-    private lateinit var mSwapChain: SwapChain
     private lateinit var mRenderer: Renderer
     private lateinit var mCamera: Camera
     private lateinit var mScene: Scene
     private lateinit var mView: View
-    private val mCanvas = Canvas()
-    private var mQuit = false
-
-    init {
-        mCanvas.run {
-            preferredSize = Dimension(kWindowWidth, kWindowHeight)
-            isVisible = true
-        }
-        JFrame(javaClass.simpleName).run {
-            addWindowListener(object : WindowAdapter() {
-                override fun windowClosing(e: WindowEvent) {
-                    println("WINDOW_CLOSING")
-                    mQuit = true
-                }
-            })
-            defaultCloseOperation = EXIT_ON_CLOSE
-            setLocationRelativeTo(null)
-            add(mCanvas)
-            pack()
-            isResizable = false
-            isVisible = true
-        }
-    }
+    private var mSwapChain: SwapChain? = null
 
     fun onCreate() {
         mEngine = Engine.create(Engine.Backend.DEFAULT)
@@ -53,24 +21,39 @@ class HelloWorld {
                 clearColor[3] = 1.0f
             }
         }
-        mSwapChain = mEngine.createSwapChain(mCanvas, SwapChain.CONFIG_DEFAULT)
         mCamera = mEngine.createCamera()
         mScene = mEngine.createScene()
         mView = mEngine.createView().apply {
-            viewport = Viewport(0, 0, kWindowWidth, kWindowHeight)
             camera = mCamera
             scene = mScene
         }
     }
 
+    fun onAttach(window: Any) {
+        check(mSwapChain == null) {
+            println("already attached")
+        }
+        mSwapChain = mEngine.createSwapChain(window, SwapChain.CONFIG_DEFAULT)
+    }
+
+    fun onResized(width: Int, height: Int) {
+        mView.viewport = Viewport(0, 0, width, height)
+    }
+
     fun onRender() {
-        while (!mQuit) {
-            if (mRenderer.beginFrame(mSwapChain, 0L)) {
+        mSwapChain?.run {
+            if (mRenderer.beginFrame(this, 0L)) {
                 mRenderer.render(mView)
                 mRenderer.endFrame()
             }
-            Thread.sleep(16)
         }
+    }
+
+    fun onDetach() {
+        mSwapChain?.run {
+            mEngine.destroySwapChain(this)
+        }
+        mSwapChain = null
     }
 
     fun onDestroy() {
@@ -79,7 +62,7 @@ class HelloWorld {
             destroyScene(mScene)
             destroyCamera(mCamera)
             destroyRenderer(mRenderer)
-            destroySwapChain(mSwapChain)
+            mSwapChain?.run { destroySwapChain(this) }
             destroy()
         }
     }
